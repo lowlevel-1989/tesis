36d179a4b3fa88226704cb21b2d80f3da431b8a4
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-02-10 15:44:01 -0430

    se creo modelo book

--- api/book/models.py
+++ api/book/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from author.models import Author
+from dewey.models import Dewey
+from publisher.models import Publisher 
 
+class Book(models.Model):
+	title = models.CharField(max_length=100)
+	author = models.ManyToManyField(Author)
+	publisher = models.ForeignKey(Publisher)
+	isbn = models.CharField(max_length=13)
+	dewey = models.ForeignKey(Dewey)
+	pages = models.IntegerField()
+	year = models.IntegerField()
+	cover = models.ImageField(upload_to = 'cover')
+
+	def __unicode__(self):
+		return '%s' % self.title
--- api/publisher/models.py
+++ api/publisher/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Publisher(models.Model):
-	name = models.CharField(max_length=30)
+	name    = models.CharField(max_length=30)
 	address = models.CharField(max_length=50)
-	city = models.CharField(max_length=60)
-	state = models.CharField(max_length=30)
+	city    = models.CharField(max_length=60)
+	state   = models.CharField(max_length=30)
 	country = models.CharField(max_length=50)
 
 	def __unicode__(self):
