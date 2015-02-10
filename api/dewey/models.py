1536d178356206f6ef2950d4ef0f794327234249
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-02-10 13:56:54 -0430

    se creo fixture dewey

--- api/dewey/models.py
+++ api/dewey/models.py
@@ -4,5 +4,10 @@ class Dewey(models.Model):
 	id          = models.CharField(max_length=3, primary_key = True)
 	description = models.CharField(max_length=45)
 
+	class Meta:
+		verbose_name		= 'Dewey'
+		verbose_name_plural	= 'Dewey'
+		ordering			= ('id',)
+
 	def __unicode__(self):
 		return '%s - %s' % ( self.id, self.description) 
