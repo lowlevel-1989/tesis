6317de67d6547ffa9507305fe68ecb87421d6961
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-02-10 15:45:46 -0430

    se arreglor error en admin book

---
+++ api/book/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('author', '0001_initial'),
+        ('dewey', '0002_auto_20150210_1918'),
+        ('publisher', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=100)),
+                ('isbn', models.CharField(max_length=13)),
+                ('pages', models.IntegerField()),
+                ('year', models.IntegerField()),
+                ('cover', models.ImageField(upload_to=b'cover')),
+                ('author', models.ManyToManyField(to='author.Author')),
+                ('dewey', models.ForeignKey(to='dewey.Dewey')),
+                ('publisher', models.ForeignKey(to='publisher.Publisher')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
