775b40180dd5976982d0a81e4c604564614e6f06
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-02-11 13:09:34 -0430

    se mejoro el admin Publisher

--- api/publisher/admin.py
+++ api/publisher/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Publisher
 
+class PublisherAdmin (admin.ModelAdmin):
+	list_display  = ('name', 'city', 'state', 'country', )
+	search_fields = ('name', 'city', 'state', 'country', )
+
 admin.site.register(Publisher)
--- api/publisher/models.py
+++ api/publisher/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Publisher(models.Model):
-	name    = models.CharField(max_length=30)
-	address = models.CharField(max_length=150)
-	city    = models.CharField(max_length=60)
-	state   = models.CharField(max_length=30)
-	country = models.CharField(max_length=50)
+	name    = models.CharField('Nombre',    max_length=30)
+	address = models.CharField('Direccion', max_length=150)
+	city    = models.CharField('Ciudad',    max_length=60)
+	state   = models.CharField('Estado',    max_length=30)
+	country = models.CharField('Pais',      max_length=50)
 
 	def __unicode__(self):
 		return '%s' % self.name 
