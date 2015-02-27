76bbdefa26b20c33ec64bc58e19d4414930a9ad4
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-02-26 20:25:08 -0430

    se agrego admin del django 1.6

--- api/biblioteca/urls.py
+++ api/biblioteca/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.contrib import admin
 from rest_framework.authtoken import views
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/',   include('author.urls')),
