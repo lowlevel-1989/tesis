dd93f259096181b65165c335a4595342e3ce6fda
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-03-01 20:13:40 -0430

    se organizo pais estado ciudad

--- biblioteca/publisher/admin.py
+++ biblioteca/publisher/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models        import Publisher
 
 class PublisherAdmin(admin.ModelAdmin):
-	list_display  = ('name', 'city', 'state', 'country', )
-	search_fields = ('name', 'city', 'state', 'country', )
+	list_display  = ('name', 'country', 'state', 'city', )
+	search_fields = ('name', 'country', 'state', 'city', )
 
 admin.site.register(Publisher, PublisherAdmin)
--- biblioteca/publisher/models.py
+++ biblioteca/publisher/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Publisher(models.Model):
 	name    = models.CharField('Editorial', max_length=30  )
 	address = models.CharField('Dirección', max_length=150 )
-	city    = models.CharField('Ciudad',    max_length=60  )
-	state   = models.CharField('Estado',    max_length=30  )
 	country = models.CharField('País',      max_length=50  )
+	state   = models.CharField('Estado',    max_length=30  )
+	city    = models.CharField('Ciudad',    max_length=60  )
 
 	class Meta:
 		verbose_name		= 'Editorial'
