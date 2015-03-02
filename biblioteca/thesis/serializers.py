ff5aa894652f0fd037cf77049d695a35555dcf64
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-03-01 20:56:49 -0430

    se serializaron las relaciones de tesis

--- biblioteca/thesis/serializers.py
+++ biblioteca/thesis/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import Thesis
+
+from author.serializers import AuthorSerializer
+
+from .models import Thesis, Careers
+
+
+class CareerSerializer(serializers.ModelSerializer):
+	class Meta:
+	    model = Careers
 
 
 class ThesisSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Thesis
+
+	author = AuthorSerializer(many=True,  read_only=True)
+	career = CareerSerializer(many=False, read_only=True)
+
+	is_public = serializers.SerializerMethodField()
+
+	def get_is_public(self, obj):
+		if obj.public:
+			return obj.thesis_pdf.url
+		return None
+
+	class Meta:
+	    model  = Thesis
+	    fields = ('title', 'author', 'career', 'year', 'cover', 'abstract_pdf', 'is_public', ) 
