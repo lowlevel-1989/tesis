740a6333a1787958dc0bde77e9d46cdef2582b67
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-03-04 00:21:11 -0430

    se serializaron las relaciones book

--- biblioteca/book/models.py
+++ biblioteca/book/models.py
@@ -11,35 +11,35 @@ from dewey.models     import Dewey
 from publisher.models import Publisher 
 
 class Book(models.Model):
-	title     = models.CharField       ( 'Titulo',               max_length=100                                                        )
-	isbn      = models.CharField       (                         max_length=13                                                         )
-	dewey     = models.ForeignKey      (              Dewey                                                                            )
-	author    = models.ManyToManyField (              Author,                   verbose_name='Autores'                                 )
-	publisher = models.ForeignKey      (              Publisher,                verbose_name='Editorial'                               )
-	pages     = models.IntegerField    ( 'Páginas'                                                                                     )
-	year      = models.IntegerField    ( 'Año'                                                                                         )
-	cover     = models.FileField       ( 'Portada',   upload_to='cover/%Y/%m/%d',     validators=[validate_file_image]                 )
-	book_pdf  =        FileField       ( 'Libro pdf', upload_to='documents/%Y/%m/%d', validators=[validate_file_extension], blank=True )
-	public    = models.BooleanField    ( 'Público',   default=False                                                                    )
+    title     = models.CharField       ( 'Titulo',               max_length=100                                                        )
+    isbn      = models.CharField       (                         max_length=13                                                         )
+    dewey     = models.ForeignKey      (              Dewey                                                                            )
+    author    = models.ManyToManyField (              Author,                   verbose_name='Autores'                                 )
+    publisher = models.ForeignKey      (              Publisher,                verbose_name='Editorial'                               )
+    pages     = models.IntegerField    ( 'Páginas'                                                                                     )
+    year      = models.IntegerField    ( 'Año'                                                                                         )
+    cover     = models.FileField       ( 'Portada',   upload_to='cover/%Y/%m/%d',     validators=[validate_file_image]                 )
+    book_pdf  =        FileField       ( 'Libro pdf', upload_to='documents/%Y/%m/%d', validators=[validate_file_extension], blank=True )
+    public    = models.BooleanField    ( 'Público',   default=False                                                                    )
 
-	class Meta:
-		verbose_name		= 'Libro'
-		verbose_name_plural	= 'Libros'
+    class Meta:
+        verbose_name        = 'Libro'
+        verbose_name_plural = 'Libros'
 
-	def pais(self):
-		return '%s' % self.publisher.country
+    def pais(self):
+        return '%s' % self.publisher.country
 
-	def portada(self):
-		if self.public and self.book_pdf.url != settings.MEDIA_URL:
-			link = self.book_pdf.url
-		else:
-			link = self.cover.url
+    def portada(self):
+        if self.public and self.book_pdf.url != settings.MEDIA_URL:
+            link = self.book_pdf.url
+        else:
+            link = self.cover.url
 
-		return """
-			<a href="%s" target="_blank"><img src="%s" width="90" height="120" /></a>
-		""" % (link, self.cover.url)
+        return """
+            <a href="%s" target="_blank"><img src="%s" width="90" height="120" /></a>
+        """ % (link, self.cover.url)
 
-	portada.allow_tags = True
+    portada.allow_tags = True
 
-	def __unicode__(self):
-		return '%s' % self.title
+    def __unicode__(self):
+        return '%s' % self.title
--- biblioteca/book/serializers.py
+++ biblioteca/book/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
+
+from author.serializers     import AuthorSerializer
+from dewey.serializers      import DeweySerializer
+from publisher.serializers  import PublisherSerializer
+
 from .models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
+    
+    author    = AuthorSerializer    (many=True,  read_only=True)
+    dewey     = DeweySerializer     (many=False, read_only=True)
+    publisher = PublisherSerializer (many=False, read_only=True)
+
     class Meta:
         model = Book
