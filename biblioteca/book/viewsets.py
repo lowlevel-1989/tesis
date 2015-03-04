0d5fe7aaa819e04f4f43a9f790cfb574bd2d6720
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-03-04 00:32:48 -0430

    se agregaron los filtros y el search a book

--- biblioteca/book/viewsets.py
+++ biblioteca/book/viewsets.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
-from .models import Book
+from rest_framework import filters
+
+
+from .models      import Book
 from .serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
     queryset            = Book.objects.all()
     serializer_class    = BookSerializer
+    filter_backends     = (filters.DjangoFilterBackend, filters.SearchFilter, )
+    filter_fields       = ('id', 'title', 'author__name', 'publisher__name', 'year', 'publisher__country', )
+    search_fields       = ('title', 'dewey__description', 'publisher__name', 'isbn', 'author__name', )
