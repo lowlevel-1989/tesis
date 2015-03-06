324a0e205c6228ad320ef17d20ead485426b779f
David Castillo <davecas26@gmail.com>
2015-03-05 22:07:45 -0430

    se cambio la cantidad de libros a mostrar en escritorio

--- web/app/shared/header/headerController.js
+++ web/app/shared/header/headerController.js
@@ -3,9 +3,11 @@
 
     .controller('headerController', [ '$scope', '$mdSidenav', 
     function ($scope, $mdSidenav) {
+
         $scope.toggleRight = function() {
     		$mdSidenav('right').toggle();
   		};
+  		
     }]);
 
 })();
