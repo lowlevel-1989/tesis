3168c38623ef64ec0243b11d3e461bbe29f5b7bd
David Castillo <davecas26@gmail.com>
2015-03-10 20:09:40 -0430

    se agregaron nuevas rutas y nuevas paginas

--- web/app/app.routes.js
+++ web/app/app.routes.js
@@ -8,6 +8,18 @@
             templateUrl: '../templates/book/bookView.min.html',
             controller: 'bookController'
         })
+        .when('/tesis', {
+            templateUrl: '../templates/tesis/tesisView.min.html',
+            controller: 'tesisController'
+        })
+        .when('/leyes', {
+            templateUrl: '../templates/leyes/leyesView.min.html',
+            controller: 'leyesController'
+        })
+        .when('/revistas', {
+            templateUrl: '../templates/revistas/revistasView.min.html',
+            controller: 'revistasController'
+        })
         .when('/home', {
             templateUrl: '../templates/home/homeView.min.html'
         })
---
+++ web/app/components/tesis/tesisService.js
@@ -0,0 +1,14 @@
+(function(){
+
+    angular.module('tesisService', [])
+
+        .factory('tesisFactory', ['$http', '$q', 'api', function ($http, $q, api){
+
+            
+            return {
+               
+            };
+
+        }]);
+    
+})();
