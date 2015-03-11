cd36fdd42ab10577b5180697bb8d7323726dc6dd
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-03-11 01:03:36 -0430

    se agrego el modulo tesis

--- web/app/components/tesis/tesisController.js
+++ web/app/components/tesis/tesisController.js
@@ -1,11 +1,71 @@
 (function(){
-    angular.module('tesisController', ['tesisService'])
+    angular.module('tesisController', ['tesisService','bookIndividualDirective'])
 
     .controller('tesisController', [ 
     '$scope', 'tesisFactory', '$rootScope',
     function ($scope, tesisFactory, $rootScope) {
         
-        
+        //BookSingle
+        $scope.bookShow = false;
+
+        $scope.showBookInfo = function(book){
+           if (!$scope.bookShow){
+                $scope.bookShow = true;
+                $scope.bookShowInfo = book;
+                console.log($scope.bookShow);
+            }else{
+                $scope.bookShow = false;
+                console.log($scope.bookShow);
+            }
+        };
+        //BookSingle
+
+
+        //Scrolling
+
+        $rootScope.$on('_change', function(event){ 
+            $scope.change = false;
+            $scope.none   = false;
+        });
+
+        $scope.scrollVisible = false;
+        var books;
+        $scope.scrollActive = function(arg){
+            if (!$scope.none){
+                $scope.scrollVisible = true;
+                var change = !$scope.change;
+                tesisFactory.next(arg, change).then(function(data){
+                    if (!$scope.change)
+                        books = data.results;
+                    else
+                        books = books.concat(data.results);
+                    $scope.change = true;
+                    $scope.books = books;                
+                    $scope.scrollVisible = false;
+                    if (data.next === null)
+                        $scope.none = true;
+                });
+            }
+        };
+
+        //Scrolling
+
+
+        //Search
+
+            var search;
+
+            $scope.search = function () {
+                if (search !== $scope.data){
+                    search = $scope.data;
+                    $scope.bookShow = false;
+                    $scope.change = false;
+                    $scope.none   = false;
+                    $scope.scrollActive(search);
+                }
+            };
+
+        //Search
     }]);
 
 })();
--- web/app/components/tesis/tesisService.js
+++ web/app/components/tesis/tesisService.js
@@ -4,9 +4,48 @@
 
         .factory('tesisFactory', ['$http', '$q', 'api', function ($http, $q, api){
 
-            
+            var paginador = api.url + 'thesis/';
+
+            function next(arg, change){
+                var page = paginador;
+                var flag;
+                var search = '';
+                if(page === null && arg === undefined)
+                    flag = true;
+
+                if (change){
+                    flag = false;
+                    page   = api.url + 'thesis/';
+                }
+
+                var deferred = $q.defer();
+                
+                if (!flag){
+                    if (arg){
+                        search = arg;
+                        page   = api.url + 'thesis/';
+                    }else if (arg === '')
+                        page   = api.url + 'thesis/';
+
+                    $http({
+                        method: 'get',
+                        url: page,
+                        params: {
+                            "search": search
+                        }
+                    })
+
+                    .success(function(data) {
+                        paginador = data.next;
+                        deferred.resolve(data);
+                    });
+                }
+
+                return deferred.promise;
+            }
+
             return {
-               
+                next: next
             };
 
         }]);
