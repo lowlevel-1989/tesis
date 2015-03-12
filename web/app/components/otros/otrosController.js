90454efb95031c5945f029da747ef1a9e60cee31
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-03-12 00:45:13 -0430

    se creo buscador generico

--- web/app/components/otros/otrosController.js
+++ web/app/components/otros/otrosController.js
@@ -4,7 +4,71 @@
     .controller('otrosController', [ 
     '$scope', 'otrosFactory', '$rootScope',
     function ($scope, otrosFactory, $rootScope) {
-    	
+    	//BookSingle
+        $scope.bookShow = false;
+
+        $scope.showBookInfo = function(book){
+           if (!$scope.bookShow){
+                $scope.bookShow = true;
+                $scope.bookShowInfo = book;
+            }else
+                $scope.bookShow = false;
+        };
+        //BookSingle
+
+        $scope.cambio = function(){
+        	otrosFactory.set($scope.select);
+        	$scope.bookShow = false;
+            $scope.change = false;
+            $scope.none   = false;
+            $scope.scrollActive(search);
+        };
+
+        //Scrolling
+
+        $rootScope.$on('_change', function(event){ 
+            $scope.change = false;
+            $scope.none   = false;
+        });
+
+        $scope.scrollVisible = false;
+        var books;
+        $scope.scrollActive = function(arg){
+            if (!$scope.none){
+                $scope.scrollVisible = true;
+                var change = !$scope.change;
+                otrosFactory.next(arg, change).then(function(data){
+                    if (!$scope.change)
+                        books = data.results;
+                    else
+                        books = books.concat(data.results);
+                    $scope.change = true;
+                    $scope.books = books;                
+                    $scope.scrollVisible = false;
+                    if (data.next === null)
+                        $scope.none = true;
+                });
+            }
+        };
+
+        //Scrolling
+
+
+        //Search
+
+            var search;
+
+            $scope.search = function () {
+                if (search !== $scope.data){
+                    search = $scope.data;
+                    $scope.bookShow = false;
+                    $scope.change = false;
+                    $scope.none   = false;
+                    $scope.scrollActive(search);
+                }
+            };
+
+        //Search
     }]);
 
 })();
--- web/app/components/otros/otrosService.js
+++ web/app/components/otros/otrosService.js
@@ -4,9 +4,64 @@
 
         .factory('otrosFactory', ['$http', '$q', 'api', function ($http, $q, api){
 
-            
-            return {
+            var actual = 'magazines/';
+            var paginador = api.url + actual;
+
+            function set(tipe){
+            	switch(tipe) {
+				    case 'Folletos':
+				    	actual = 'brochures/';
+				        break;
+				    case 'Leyes':
+				    	actual = 'laws/';
+				        break;
+				    default:
+				    	actual = 'magazines/';
+				}
+				paginador = api.url + actual;
+            }
+
+            function next(arg, change){
+                var page = paginador;
+                var flag;
+                var search = '';
+                if(page === null && arg === undefined)
+                    flag = true;
+
+                if (change){
+                    flag = false;
+                    page   = api.url + actual;
+                }
+
+                var deferred = $q.defer();
                 
+                if (!flag){
+                    if (arg){
+                        search = arg;
+                        page   = api.url + actual;
+                    }else if (arg === '')
+                        page   = api.url + actual;
+
+                    $http({
+                        method: 'get',
+                        url: page,
+                        params: {
+                            "search": search
+                        }
+                    })
+
+                    .success(function(data) {
+                        paginador = data.next;
+                        deferred.resolve(data);
+                    });
+                }
+
+                return deferred.promise;
+            }
+
+            return {
+                next: next,
+                set: set 
             };
 
         }]);
